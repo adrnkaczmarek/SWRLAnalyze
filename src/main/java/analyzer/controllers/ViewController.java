36834cac021738d2dd9df6b2ffd548b20c117844
adrnkaczmarek <adrn.kaczmarek@gmail.com>
2017-01-09 18:48:15 +0100

    bug fix with filter parameter

--- src/main/java/analyzer/controllers/ViewController.java
+++ src/main/java/analyzer/controllers/ViewController.java
@@ -4,6 +4,9 @@ import analyzer.utils.SWRLReader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class ViewController
@@ -16,17 +19,28 @@ public class ViewController
         reader = new SWRLReader(
                 "C:\\Users\\adrn.kaczmarek\\IdeaProjects\\SWRLAnalyze\\src\\main\\resources\\family.xml");
         String[] rules = reader.getRules().toArray(new String[0]);
-        String[] classes = reader.getClasses().toArray(new String[0]);
+        List<String> classes_list = reader.getClasses();
+        classes_list.add(0, "None");
+        String[] classes = classes_list.toArray(new String[0]);
         model.addAttribute("classes", classes);
         model.addAttribute("rules", rules);
         return "index";
     }
 
     @RequestMapping("/filter")
-    public String filter(Model model, String filter)
+    public String filter(Model model, @RequestParam("filter") String filter)
     {
         /*TODO: Tu wczytaj przefiltrowane metody do zmiennej 'rules'*/
         String[] rules = {"example"};
+
+        if(filter.equals("None"))
+        {
+
+        }
+        else
+        {
+
+        }
         model.addAttribute("rules", rules);
         return "swrl_list";
     }
--- src/main/resources/js/main.js
+++ src/main/resources/js/main.js
@@ -4,13 +4,14 @@ $(function () {
 
     $(".dropdown-menu li a").click(function(){
 
-        $(this).parents(".dropdown").children(".dropdown-toggle").text($(this).text());
+        var text = $(this).text();
+        $(this).parents(".dropdown").children(".dropdown-toggle").text(text);
         selected_class = $(this).text();
     });
 
     $("#filter_post").click(function () {
         var button = $(this);
-        $.post("filter", selected_class,
+        $.post("filter", {filter: selected_class},
             function(data, status)
             {
                 $("#swrl_list").html(data);
